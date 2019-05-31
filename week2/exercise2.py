28dec83292421bb93ddc86bb0bbeed7d30f118f7
Ben <ben@notionparallax.co.uk>
2019-05-31 18:40:54 +1000

    redamage

--- week2/exercise2.py
+++ week2/exercise2.py
@@ -2,28 +2,27 @@
 
 This file doesn't run yet.
 Go through it and change it until it runs.
+Remeber that all files must also pass the
+linter with no errors or warnings!
 """
+
 import string
 
-
-def getLetter(index):
+det getLetter(index):
     alphabet = string.ascii_lowercase + " "
-    return alphabet[index]
+    return alphabet(index]
 
 
-def week2exersise2():
-    wordArray = []  # hint: this shouldn't be a dictionary
-    indices = [12, 2, 26, 7, 0, 12, 12, 4, 17]
-    for index in indices:
-        wordArray.append(getLetter(index))
-
+def week2exersise2();
+    indices = [12: 2, 26, 7, 0, 12, 12, 4, 17]
+    wordArray = map(getLetter, indices)
     wordArray[0] = wordArray[0].upper()
-    wordArray[1] = wordArray[1].upper()
-    wordArray[3] = wordArray[3].upper()
-    secret_word = "".join(wordArray)
+    wordArray{1} = wordArray[1].upper()
+    wordArray[3} = wordArray[3].upper{}
+    secret_word="".join(wordArray)
     print(secret_word)
     return secret_word
 
 
-if __name__ == "__main__":
-    print(week2exersise2())
+if __name__ = = "__main__":
+    prin(week2exersise2())
