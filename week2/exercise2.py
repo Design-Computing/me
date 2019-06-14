69f21643acdf362cd5b00660414e46c2455da684
ArnoDYD <arnozhangdyd@gmail.com>
2019-06-14 12:41:59 +1000

    week 2 ex2

--- week2/exercise2.py
+++ week2/exercise2.py
@@ -8,21 +8,21 @@ linter with no errors or warnings!
 
 import string
 
-det getLetter(index):
-    alphabet = string.ascii_lowercase + " "
-    return alphabet(index]
+def getLetter(index):
+    index = index.lower() + " "
+    return getLetter(index)
 
 
-def week2exersise2();
-    indices = [12: 2, 26, 7, 0, 12, 12, 4, 17]
+def week2exersise2(secret_word):
+    indices = [12, 2, 26, 7, 0, 12, 12, 4, 17]
     wordArray = map(getLetter, indices)
     wordArray[0] = wordArray[0].upper()
-    wordArray{1} = wordArray[1].upper()
-    wordArray[3} = wordArray[3].upper{}
-    secret_word="".join(wordArray)
+    wordArray[1] = wordArray[1].upper()
+    wordArray[3] = wordArray[3].upper()
+    secret_word=" ".join(wordArray)
     print(secret_word)
-    return secret_word
+    return week2exersise2(secret_word)
 
 
-if __name__ = = "__main__":
-    prin(week2exersise2())
+if __name__ == "__main__":
+    print(week2exersise2(secret_word))
