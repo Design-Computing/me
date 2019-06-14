dc28e144b4f4b113772fcfe12b277aa54cac44a4
ArnoDYD <arnozhangdyd@gmail.com>
2019-06-14 13:05:09 +1000

    week 2 ex 1

--- week2/exercise1.py
+++ week2/exercise1.py
@@ -11,25 +11,36 @@ import platform
 # I think this will print "hello! Let's get started" by calling the print function.
 print("hello! Let's get started")  # it printed "hello! Let's get started"
 
-some_words = ['what', 'does', 'this', 'line', 'do', '?']
+# I think this have the collections of the word ('what', 'does', 'this', 'line', 'do', '?')
+some_words = ['what', 'does', 'this', 'line', 'do', '?'] #This shows the collections have the words ('what', 'does', 'this', 'line', 'do', '?')
 
-for word in some_words:
-    print(word)
+# I think it is looking for 'word' in this collection.
+for word in some_words: # it creates a loop called word with the list of 'some_words'
+    # I think this print the 'word' loop which is the list/collection in the collection.
+    print(word) # This line prints the 'word' loop which is the list/collection in the collection.
 
-for x in some_words:
-    print(x)
+# I think it creates a loop called x with the list called 'some_words'
+for x in some_words: # it creates a loop called word.
+    # I think this print the 'x' loop which is the list/collection in the collection.
+    print(x) # This line prints the 'x' loop which is the list/collection in the collection.
 
-print(some_words)
+# I think this print the collection called 'some_words'.
+print(some_words) # It prints the collection itself.
 
-if len(some_words) > 3:
-    print('some_words contains more than 3 words')
+# I think it checkes whether the collection 'some_words' contains more than 3 conponents.
+if len(some_words) > 3: # It checkes whether the collection 'some_words' contains more than 3 conponents.
+    # I think it prints 'some_words contains more than 3 words'.
+    print('some_words contains more than 3 words') # It prints 'some_words contains more than 3 words'.
 
-def usefulFunction():
+# I think it defines the function of 'usefulFunction()'
+def usefulFunction(): # It defines the function of 'usefulFunction()'
     """
     You may want to look up what uname does before you guess
     what the line below does:
     https://docs.python.org/3/library/platform.html#platform.uname
     """
-    print(platform.uname())
+    # I think it prints out the content of 'platform.uname()' which is: system, node, release, version, machine, and processor.
+    print(platform.uname()) # It prints out the content of 'platform.uname()' which is: system, node, release, version, machine, and processor to the function 'usefulFunction() defined above.
 
-usefulFunction()
+# This line prints the function called 'usefulFunction()'
+usefulFunction() # It prints the function called 'usefulFunction()'.
