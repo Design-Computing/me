71bfffc9c56877e5cdd89649c54aaa2a093fb292
Ben Doherty <ben@notionparallax.co.uk>
2023-05-29 17:07:05 +1000

    do set 1

--- set1/exercise1.py
+++ set1/exercise1.py
@@ -8,5 +8,5 @@ https://design-computing.github.io/md/week1
 
 """
 # 👇👇 Write your code here 👇👇
-
+print("Hello world!")
 # 👆👆 Write your code here 👆👆
--- set1/readme.md
+++ set1/readme.md
@@ -1,1 +1,3 @@
 TODO: Reflect on what you learned this week and what is still unclear.
+
+hello, I am verty smart
